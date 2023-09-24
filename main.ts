e403bb3d9a099e48cde2866ad8bd861acb1d0010
doroteablanco <130961780+doroteablanco@users.noreply.github.com>
2023-09-24 22:09:40 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,35 +8,52 @@ datalogger.onLogFull(function () {
         # # # # #
         `)
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue(log_control, 1)
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue(log_control, 2)
+    basic.showIcon(IconNames.No)
+})
 radio.onReceivedValue(function (name, value) {
-    let log_control = 0
-    let angle_signal = 0
     if (name == angle_signal) {
-        let _4digit: grove.TM1637 = null
         if (log_indicator) {
             datalogger.log(datalogger.createCV("Angle Value - Actuator 2", value))
             led.toggle(4, 4)
         }
-        _4digit.show(value)
+        display.show(value)
     }
     if (name == log_control) {
         if (value == 1) {
             log_indicator = true
+            timeanddate.set24HourTime(0, 0, 0)
             basic.showIcon(IconNames.Yes)
-        } else {
+        } else if (value == 2) {
             log_indicator = false
             basic.showIcon(IconNames.No)
+        } else {
+        	
         }
     }
 })
+let angle_signal = ""
+let log_control = ""
 let log_indicator = false
+let display: grove.TM1637 = null
 datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
 timeanddate.set24HourTime(0, 0, 0)
 radio.setGroup(100)
-let display = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
+display = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
 log_indicator = false
 datalogger.log(datalogger.createCV("Angle Value - Actuator 2", 0))
 basic.showIcon(IconNames.No)
 basic.forever(function () {
-	
+    radio.sendValue(angle_signal, pins.map(
+    0,
+    0,
+    1023,
+    0,
+    180
+    ))
 })
